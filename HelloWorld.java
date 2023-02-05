9a8af44db1aa4168c397ded09f467cda4e5119f0
Srinidhi-N-K <121165476+Srinidhi-N-K@users.noreply.github.com>
2023-02-05 15:25:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("------Hello--world!-----");
-     System.out.println("-----Hello--world!-----");
-     System.out.println("-----Hello--world!-----");
+     System.out.println("-----Hello-World is boring let's keep-----");
+     System.out.println("-----Hai Baby!!-----");
   }
 }
